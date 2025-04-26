871fdff3dd4f265caf6f3b5dc501175e7349373a
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-04-27 00:33:33 +0530

    store has been made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+      console.log("User data: ", userData)
+    })
+    .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <>
-      <h1> Blog with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
